5e94af4b61120666a5a331d7f5781859927ae864
Ziad Allam <ziadmahmoudallam@gmail.com>
2026-02-20 15:03:08 +0200

    updated grid cols on mobile to be 2

--- frontend/src/pages/products/ProductsList.jsx
+++ frontend/src/pages/products/ProductsList.jsx
@@ -50,7 +50,7 @@ function ProductsList() {
                         <SortOptions />
                     </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 py-3">
+                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 py-3">
                         {isLoading ? (
                             Array.from({ length: 20 }).map((_, i) => (
                                 <ProductCardLoading key={i} />
